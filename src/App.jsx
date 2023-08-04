bd5cff4dd616a3ad1f999ccb11e847eaac523e17
CristoferNava <cristofernava11719@gmail.com>
2023-08-04 17:41:26 -0600

    Layout was created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+// 1. Build the layout
+
+import { useState } from "react";
+
+import "./App.css";
+
 function App() {
-  return <h1>Ready to work</h1>;
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+  const [squares, setSquares] = useState(new Array(numbers.length).fill(null));
+
+  return (
+    <main className="main">
+      <h1 className="main-title">Ordena los números</h1>
+
+      <div className="numbers-container">
+        {numbers.map((number) => {
+          return <Number key={number} value={number} />;
+        })}
+      </div>
+
+      <div className="squares-container">
+        {squares.map((square, idx) => {
+          return <Square key={idx} />;
+        })}
+      </div>
+    </main>
+  );
 }
+
+const Number = ({ value }) => {
+  return <div className="number">{value}</div>;
+};
+
+const Square = () => {
+  return <div className="square"></div>;
+};
 
 export default App;
