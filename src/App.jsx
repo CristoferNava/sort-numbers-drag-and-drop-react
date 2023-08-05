106235da51b77051f4010e61e237708dda1d8687
CristoferNava <cristofernava11719@gmail.com>
2023-08-04 19:12:23 -0600

    Drag the numbers to squares functionality was added.

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,56 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
-  const [squares, setSquares] = useState(new Array(numbers.length).fill(null));
+  const [numbers, setNumbers] = useState(
+    new Map([
+      [2, 2],
+      [1, 1],
+      [3, 3],
+      [5, 5],
+      [4, 4],
+    ])
+  );
+  const [squares, setSquares] = useState(
+    new Array([...numbers.values()].length).fill(null)
+  );
 
+  console.log(squares);
   return (
     <main className="main">
       <h1 className="main-title">Ordena los números</h1>
 
       <div className="numbers-container">
-        {numbers.map((number) => {
+        {[...numbers.values()].map((number) => {
           return <Number key={number} value={number} />;
         })}
       </div>
 
       <div className="squares-container">
-        {squares.map((square, idx) => {
-          return <Square key={idx} />;
+        {squares.map((squareVal, idx) => {
+          if (squareVal != null) {
+            return (
+              <Square
+                key={idx}
+                id={idx}
+                squares={squares}
+                setSquares={setSquares}
+                numbers={numbers}
+                setNumbers={setNumbers}
+              >
+                <Number value={squareVal} />
+              </Square>
+            );
+          }
+          return (
+            <Square
+              key={idx}
+              id={idx}
+              squares={squares}
+              setSquares={setSquares}
+              numbers={numbers}
+              setNumbers={setNumbers}
+            />
+          );
         })}
       </div>
 
@@ -30,11 +64,46 @@ function App() {
 }
 
 const Number = ({ value }) => {
-  return <div className="number">{value}</div>;
+  const dragStart = (event, number) => {
+    event.dataTransfer.setData("text/plain", number);
+  };
+
+  return (
+    <div
+      className="number"
+      draggable={true}
+      onDragStart={(e) => dragStart(e, value)}
+    >
+      {value}
+    </div>
+  );
 };
 
-const Square = () => {
-  return <div className="square"></div>;
+const Square = ({ id, children, squares, setSquares, numbers, setNumbers }) => {
+  const dragOver = (event) => {
+    event.preventDefault();
+    console.log("You are dragging over me");
+  };
+
+  const drop = (event, squareIdx) => {
+    console.log("You dropped over me, ", squareIdx);
+    const numberToAdd = parseInt(event.dataTransfer.getData("text/plain"));
+    console.log(numberToAdd);
+
+    // Add the number to the squareIdx
+    squares[squareIdx] = numberToAdd;
+    setSquares([...squares]);
+
+    // Remove the number of the numbers
+    numbers.delete(numberToAdd);
+    setNumbers(new Map(numbers));
+  };
+
+  return (
+    <div className="square" onDragOver={dragOver} onDrop={(e) => drop(e, id)}>
+      {children}
+    </div>
+  );
 };
 
 export default App;
